37925a891844eb36d51bc20ab875dc8a18367bd0
michaeljohnbrown3 <michaeljohnbrown3@gmail.com>
2023-06-09 18:24:19 -0400

    created star rating component

---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export function NavBar({ children }) {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      {children}
+    </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
+
+export function Search() {
+  const [query, setQuery] = useState("");
+
+  return <input className="search" type="text" placeholder="Search movies..." value={query} onChange={(e) => setQuery(e.target.value)} />;
+}
+
+export function NumResults({ movies }) {
+  return (
+    <p className="num-results">
+      Found <strong>{movies.length}</strong> results
+    </p>
+  );
+}
